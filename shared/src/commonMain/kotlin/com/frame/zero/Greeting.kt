7aee40a26af437745de24e0071cf8880ca07fc16
Ruslan Andriievskyi <ruslan.andriievskyi@vista.com>
2026-04-26 17:41:56 +0200

    Clean up

--- shared/src/commonMain/kotlin/com/frame/zero/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.frame.zero
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- shared/src/jsMain/kotlin/com/frame/zero/Platform.js.kt
+++
@@ -1,7 +0,0 @@
-package com.frame.zero
-
-class JsPlatform: Platform {
-    override val name: String = "Web with Kotlin/JS"
-}
-
-actual fun getPlatform(): Platform = JsPlatform()
