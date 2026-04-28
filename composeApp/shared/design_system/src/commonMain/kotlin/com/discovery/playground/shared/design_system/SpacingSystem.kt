189ba3c5d00d14c25bb2bd45116e188c96807f96
Ruslan Andriievskyi <ruslan.andriievskyi@vista.com>
2026-04-28 21:25:14 +0200

    Design system tokens

--- composeApp/shared/design_system/src/commonMain/kotlin/com/discovery/playground/shared/design_system/SpacingSystem.kt
+++ composeApp/shared/design_system/src/commonMain/kotlin/com/discovery/playground/shared/design_system/SpacingSystem.kt
@@ -5,9 +5,15 @@ import androidx.compose.ui.unit.Dp
 
 @Immutable
 data class SpacingSystem(
-  val space2: Dp,
-  val space4: Dp,
-  val space8: Dp,
-  val space16: Dp,
-  val space24: Dp,
+  val xxs: Dp,
+  val xs: Dp,
+  val sm: Dp,
+  val md: Dp,
+  val lg: Dp,
+  val xl: Dp,
+  val xxl: Dp,
+  val x3l: Dp,
+  val x4l: Dp,
+  val x5l: Dp,
+  val x6l: Dp,
 )
