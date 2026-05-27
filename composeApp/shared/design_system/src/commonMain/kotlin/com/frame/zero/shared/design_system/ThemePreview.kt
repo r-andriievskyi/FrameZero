b7f8acf9e6b2e37306b8efcdba6bc5fc64ecdc8c
Ruslan Andriievskyi <andrievskiy.r@gmail.com>
2026-05-27 12:35:16 +0200

    Light and dark theme preview annotation

---
+++ composeApp/shared/design_system/src/commonMain/kotlin/com/frame/zero/shared/design_system/ThemePreview.kt
@@ -0,0 +1,11 @@
+package com.frame.zero.shared.design_system
+
+import androidx.compose.ui.tooling.preview.AndroidUiModes.UI_MODE_NIGHT_NO
+import androidx.compose.ui.tooling.preview.AndroidUiModes.UI_MODE_NIGHT_YES
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(name = "Light", uiMode = UI_MODE_NIGHT_NO)
+@Preview(name = "Dark", uiMode = UI_MODE_NIGHT_YES)
+annotation class LightDarkPreview
+
+
