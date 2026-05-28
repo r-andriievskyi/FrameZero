eb2f449f44d6f7289bceadc26db59229cd1f5e72
Ruslan Andriievskyi <andrievskiy.r@gmail.com>
2026-05-28 11:59:43 +0200

    Account screen UI 1

--- shared/features/account/src/commonMain/kotlin/com/frame/zero/feature/account/AccountComponent.kt
+++ shared/features/account/src/commonMain/kotlin/com/frame/zero/feature/account/AccountComponent.kt
@@ -1,8 +1,40 @@
 package com.frame.zero.feature.account
 
 import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.essenty.instancekeeper.getOrCreate
+import kotlinx.coroutines.flow.StateFlow
 
 class AccountComponent(
   componentContext: ComponentContext,
-  val onBack: () -> Unit
-) : ComponentContext by componentContext
+  val onBack: () -> Unit,
+  viewModelFactory: () -> AccountViewModel
+) : ComponentContext by componentContext {
+  private val viewModel: AccountViewModel = instanceKeeper.getOrCreate { viewModelFactory() }
+
+  val state: StateFlow<AccountState>
+    get() = viewModel.state
+
+  fun onEditProfileClick() {
+    // TODO: navigate to edit profile
+  }
+
+  fun onEmailClick() {
+    // TODO: navigate to email settings
+  }
+
+  fun onPasswordSecurityClick() {
+    // TODO: navigate to password & security
+  }
+
+  fun onNotificationsClick() {
+    // TODO: navigate to notification settings
+  }
+
+  fun onAboutClick() {
+    // TODO: navigate to about
+  }
+
+  fun onSignOutClick() {
+    // TODO: trigger sign-out
+  }
+}
--- shared/features/account/src/commonMain/kotlin/com/frame/zero/feature/account/AccountModule.kt
+++ shared/features/account/src/commonMain/kotlin/com/frame/zero/feature/account/AccountModule.kt
@@ -3,4 +3,5 @@ package com.frame.zero.feature.account
 import org.koin.dsl.module
 
 val featureAccountModule = module {
+  factory { AccountViewModel(get()) }
 }
