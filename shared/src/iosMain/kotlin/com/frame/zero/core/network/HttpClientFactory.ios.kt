800ec3c24d0e5c14a998f15c13d76ab135b88f14
Ruslan Andriievskyi <ruslan.andriievskyi@vista.com>
2026-06-02 11:01:55 +0200

    Http clients' tweaks

--- shared/src/iosMain/kotlin/com/frame/zero/core/network/HttpClientFactory.ios.kt
+++ shared/src/iosMain/kotlin/com/frame/zero/core/network/HttpClientFactory.ios.kt
@@ -3,4 +3,9 @@ package com.frame.zero.core.network
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.darwin.Darwin
 
-internal actual fun httpClientEngine(): HttpClientEngine = Darwin.create()
+internal actual fun httpClientEngine(): HttpClientEngine = Darwin.create {
+  configureRequest {
+    setAllowsCellularAccess(true)
+    setTimeoutInterval(30.0)
+  }
+}
