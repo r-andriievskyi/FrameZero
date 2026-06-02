1054740f63e7519f7bfaba87857cdf7bb720fb13
Ruslan Andriievskyi <ruslan.andriievskyi@vista.com>
2026-06-02 11:01:55 +0200

    Http clients' tweaks

--- shared/src/androidMain/kotlin/com/frame/zero/core/network/HttpClientFactory.android.kt
+++ shared/src/androidMain/kotlin/com/frame/zero/core/network/HttpClientFactory.android.kt
@@ -5,10 +5,15 @@ import com.chuckerteam.chucker.api.ChuckerInterceptor
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.okhttp.OkHttp
 import org.koin.mp.KoinPlatform
+import java.util.concurrent.TimeUnit
 
 internal actual fun httpClientEngine(): HttpClientEngine {
   val context = KoinPlatform.getKoin().get<Context>()
   return OkHttp.create {
+    config {
+      connectTimeout(15, TimeUnit.SECONDS)
+      readTimeout(30, TimeUnit.SECONDS)
+    }
     addInterceptor(ChuckerInterceptor.Builder(context).build())
   }
 }
