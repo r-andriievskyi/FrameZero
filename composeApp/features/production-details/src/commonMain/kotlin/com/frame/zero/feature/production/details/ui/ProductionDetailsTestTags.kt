0a38c3be473dc24ea3de2446434c679f76cfe451
Ruslan Andriievskyi <andrievskiy.r@gmail.com>
2026-06-28 16:48:34 +0200

    Production details UI tests

---
+++ composeApp/features/production-details/src/commonMain/kotlin/com/frame/zero/feature/production/details/ui/ProductionDetailsTestTags.kt
@@ -0,0 +1,7 @@
+package com.frame.zero.feature.production.details.ui
+
+internal object ProductionDetailsTestTags {
+  const val LOADING = "production-details:loading"
+  const val ERROR = "production-details:error"
+  const val CONTENT = "production-details:content"
+}
