06e3593aafc6c7b73f6c20208fd5a3110fa54db9
ruslan <andrievskiy.r@gmail.com>
2026-09-15 23:56:44 +0200

    Removed chat feature 2

--- shared/test-fixtures/build.gradle.kts
+++ shared/test-fixtures/build.gradle.kts
@@ -15,7 +15,6 @@ kotlin {
       api(projects.shared.repositories.productions.api)
       api(projects.shared.repositories.schedule)
       api(projects.shared.repositories.tasks.api)
-      api(projects.shared.repositories.chat.api)
       api(projects.shared.repositories.forceUpdate.api)
       api(libs.androidx.paging.common)
       api(libs.ktor.clientMock)
